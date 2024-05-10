59d664c01676e3ec6e055fc1071bbae74826e46c
Victor Navarro <victor.navarro.xyz@gmail.com>
2024-05-10 23:39:00 +0200

    fix view composable and fonts

--- composables/view.ts
+++ composables/view.ts
@@ -1,45 +1,45 @@
-type ViewState = {
-    width: number,
-    hieght: number,
-
-    /* Body font-size in pixels */
-    rem: number,
-}
-
-
 export const useView = () =>
-{
-    const hooked = useState('view-state-hooked', () => false);
-    const ready = useState('view-state-ready', () => false);
-    const state = useState<ViewState>('view-state', () => <ViewState>{ });
-    if (!hooked.value && !process.server)
     {
-        hooked.value = true;
-        onMounted(() =>
-        {
-            state.value.rem = parseInt(
-                getComputedStyle(document.documentElement).fontSize
-            );
-            const onResize = () =>
+        const hooked = useState('view-state-hooked', () => false);
+        const ready = useState('view-state-ready', () => false);
+        
+        const width = useState('view-state-w', () => 0);
+        const height = useState('view-state-h', () => 0);
+        const rem = useState('view-state-rem', () => 10);
+    
+    
+        if (!hooked.value && !process.server)
+        {        
+            hooked.value = true;
+            onMounted(() =>
             {
-                state.value.width = window.innerWidth;
-                state.value.hieght = window.innerHeight;
-                
-                const vh = window.innerHeight * 0.01;
-                document.documentElement.style.setProperty('--vh', `${vh}px`);
-            }
-            if (window)
-            {                
-                window.addEventListener('resize', onResize);
-                window.addEventListener('beforeunload', () => window.scrollTo(0, 0));
-                onResize();
-                requestAnimationFrame(() => ready.value = true);
-            }
-        });
+                rem.value = parseInt(
+                    getComputedStyle(document.documentElement).fontSize
+                );
+                const onResize = () =>
+                {
+                    width.value = window.innerWidth;
+                    height.value = window.innerHeight;
+                    
+                    const vh = window.innerHeight * 0.01;
+                    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    
+                    // const navbarH = document.querySelector('nav.c-navbar')!.clientHeight;
+                    // document.documentElement.style.setProperty('--navbarH', `${navbarH}px`);
+                }
+                if (window)
+                {                
+                    window.addEventListener('resize', onResize);
+                    window.addEventListener('beforeunload', () => window.scrollTo(0, 0));
+                    onResize();
+                    requestAnimationFrame(() => ready.value = true);
+                }
+            });
+        }
+        const isThin = (w: number) => width.value <= w;
+        const isShort = (h: number) => height.value <= h;
+    
+    
+        return { ready, isThin, isShort, width, height, rem };
     }
-    const isThin = (w: number) => state.value.width <= w;
-    const isShort = (h: number) => state.value.hieght <= h;
-
-
-    return { ready, dims: state, isThin, isShort };
-}
+    
